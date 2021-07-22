9259e4fab4ff640c319da4c91409c662af7b4abb
programador-negro <programador.negro@gmail.com>
2021-07-21 20:16:35 -0500

    se agrupa en carpeta python

--- consecutives_values.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    def consecution(n):
-        nums = list(range(1,(n+1)))
-        onum=str()
-        for x in nums:
-            onum+=str(x)
-        print(onum)
-    consecution(n)
